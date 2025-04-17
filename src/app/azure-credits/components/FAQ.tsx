a5e5331c271b4c4764808f660aa7d3b38578700a
Jeff Wilcox <427913+jeffwilcox@users.noreply.github.com>
2025-04-17 19:53:58 +0000

    Updated paragraphs

--- src/app/azure-credits/components/FAQ.tsx
+++ src/app/azure-credits/components/FAQ.tsx
@@ -16,17 +16,30 @@ const AzureCreditsQuestions = () => {
         <li>
           <b>What is this program?</b>
           <br />
-          Microsoft offers grants of Azure credits to open source projects to help with their short-term development, infrastructure, and testing needs. Workloads such as long-term website hosting, continuous integration, and permanent database hosting are not a good fit for grant programs.
+          {`
+          Microsoft offers grants of Azure credits to open source projects to help 
+          with their short-term development, infrastructure, and testing needs. 
+          Workloads such as long-term website hosting, continuous integration, and 
+          permanent database hosting are not a good fit for grant programs.
+          `}
         </li>
         <li>
           <b>Why does Microsoft provide Azure credits?</b>
           <br />
-          Microsoft recognizes the value of contributing to communities, and we are in a position to provide grants to support the ecosystem. Our product and service teams often build strategic relationships with communities and are able to choose to provide credit as a result.
+          {`
+          Microsoft recognizes the value of contributing to communities, and 
+          we are in a position to provide grants to support the ecosystem. Our 
+          product and service teams often build strategic relationships with 
+          communities and are able to choose to provide credit as a result.
+          `}
         </li>
         <li>
           <b>What are the grants for and how long do they last?</b>
           <br />
-          Most grants span a single Microsoft fiscal year, but strategic relationships with specific businesses may be available on other terms.
+          {`
+          Most grants span a single Microsoft fiscal year, but strategic 
+          relationships with specific businesses may be available on other terms.
+          `}
         </li>
       </ul>
     </div>
--- src/app/azure-credits/components/Overview.tsx
+++ src/app/azure-credits/components/Overview.tsx
@@ -10,16 +10,20 @@ const AzureCreditsOverview = () => {
         Azure credits for open source projects
       </h4>
       <p>
+        {`
         Many Microsoft product teams grant Azure credits to open source communities 
         to support their work. These credits augment other complementary programs
         such as GitHub repositories, CI/CD with GitHub Actions, as well as financial 
         contributions through the Microsoft FOSS Fund, foundation memberships, and
         other ecosystem support.
+        `}
       </p>
       <p>
+        {`
         Microsoft not currently accepting new applications from projects directly and 
         is instead relying on strategic, organic relationships between Microsoft
         businesses and the open source that they depend on.
+        `}
       </p>
     </div>
   );
