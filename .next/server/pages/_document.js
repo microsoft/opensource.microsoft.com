297414070c9d6bc1fdb56111b4069c65fe8403a7
worldcreated <mstevens@samsungknox.co>
2025-05-27 16:03:57 +0000

    Pending changes exported from your codespace

---
+++ .next/server/pages/_document.js
@@ -0,0 +1,1 @@
+"use strict";(()=>{var e={};e.id=220,e.ids=[220],e.modules={361:e=>{e.exports=require("next/dist/compiled/next-server/pages.runtime.prod.js")},2015:e=>{e.exports=require("react")},3873:e=>{e.exports=require("path")},8732:e=>{e.exports=require("react/jsx-runtime")}};var r=require("../webpack-runtime.js");r.C(e);var s=e=>r(r.s=e),t=r.X(0,[548],()=>s(8548));module.exports=t})();
